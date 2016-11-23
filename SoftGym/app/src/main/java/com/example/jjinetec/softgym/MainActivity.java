49eda0a11942b3e0bfa227d4773cf1e9c196eb71
Carlos A <darkklitos@gmail.com>
2016-11-23 12:48:14 -0500

    Se integra firebase y Login en la app

--- SoftGym/app/src/main/java/com/example/jjinetec/softgym/MainActivity.java
+++ SoftGym/app/src/main/java/com/example/jjinetec/softgym/MainActivity.java
@@ -2,50 +2,194 @@ package com.example.jjinetec.softgym;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
 
-    private Button mButtonExercise;
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        mButtonExercise = (Button) findViewById(R.id.buttonEjercicio);
+	@BindView( R.id.btnPrueba )
+	Button btnPrueba;
 
-        mButtonExercise.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this, DescriptionExercise.class);
-                startActivity(i);
-            }
-        });
 
-       // Button button = (Button) findViewById(R.id.btnVista);
-       // button.setOnClickListener(new View.OnClickListener() {
-          // public void onClick(View v) {
-          //      // Perform action on click
-         //      // setContentView(R.layout.login);
-         //   }
+	private FirebaseAuth mAuth;
+	private final String TAG = "MAIN_ACTIVITY";
+	private FirebaseAuth.AuthStateListener mAuthListener;
 
-        // });
+	private final int RC_SIGN_IN = 1;
+	private GoogleApiClient mGoogleApiClient;
 
-    }
-    public void lanzar(View view){
-        Toast.makeText(MainActivity.this, "Llego", Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(this,Login2.class);
-        startActivity(i);
-    }
-    public void lanzare(View view){
-        Toast.makeText(MainActivity.this, "Reportes", Toast.LENGTH_SHORT).show();
-        Intent i = new Intent(this,GraphicsReports.class);
-        startActivity(i);
-    }
 
+	private Button mButtonExercise;
+	private Button ingresarBtn;
+	private SignInButton mBtnGoogle;
+
+
+	@Override
+	protected void onCreate ( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		ButterKnife.bind( this );
+//        setContentView(R.layout.activity_main);
+		setContentView( R.layout.activity_login2 );
+		mAuth = FirebaseAuth.getInstance( );
+
+
+		// Configurar Login Google
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder( GoogleSignInOptions.DEFAULT_SIGN_IN )
+						.requestIdToken( getString( R.string.default_web_client_id ) )
+						.requestEmail( )
+						.build( );
+
+		mBtnGoogle = (SignInButton ) findViewById( R.id.btnSignIn );
+		mBtnGoogle.setOnClickListener( new View.OnClickListener(){
+
+			@Override
+			public void onClick ( View view ) {
+				signIn();
+			}
+		} );
+
+
+		mGoogleApiClient = new GoogleApiClient.Builder( getApplicationContext( ) )
+						.enableAutoManage( this, new GoogleApiClient.OnConnectionFailedListener( ) {
+							@Override
+							public void onConnectionFailed ( @NonNull ConnectionResult connectionResult ) {
+								Toast.makeText( MainActivity.this, "Se presento un error con el Login", Toast.LENGTH_SHORT ).show( );
+							}
+						} )
+						.addApi( Auth.GOOGLE_SIGN_IN_API, gso )
+						.build( );
+
+//		mButtonExercise = ( Button ) findViewById( R.id.buttonEjercicio );
+//
+//		mButtonExercise.setOnClickListener( new View.OnClickListener( ) {
+//			@Override
+//			public void onClick ( View v ) {
+//				Intent i = new Intent( MainActivity.this, DescriptionExercise.class );
+//				startActivity( i );
+//			}
+//		} );
+
+		mAuthListener = new FirebaseAuth.AuthStateListener( ) {
+			@Override
+			public void onAuthStateChanged ( @NonNull FirebaseAuth firebaseAuth ) {
+				FirebaseUser user = firebaseAuth.getCurrentUser( );
+				if ( user != null ) {
+					// User is signed in
+					Log.d( TAG, "onAuthStateChanged:signed_in:" + user.getUid( ) );
+					Log.d( TAG, "Ingreso Correctamente :)");
+					startActivity( new Intent( MainActivity.this, GraphicsReports.class ) );
+				}
+				else {
+					// User is signed out
+					Log.d( TAG, "onAuthStateChanged:signed_out" );
+				}
+			}
+		};
+
+
+	}
+
+	@Override
+	public void onStart ( ) {
+		super.onStart( );
+		mAuth.addAuthStateListener( mAuthListener );
+	}
+
+	@Override
+	public void onStop ( ) {
+		super.onStop( );
+		if ( mAuthListener != null ) {
+			mAuth.removeAuthStateListener( mAuthListener );
+		}
+	}
+
+
+	public void lanzar ( View view ) {
+		Toast.makeText( MainActivity.this, "Llego", Toast.LENGTH_SHORT ).show( );
+		Intent i = new Intent( this, Login2.class );
+		startActivity( i );
+	}
+
+	public void lanzare ( View view ) {
+		Toast.makeText( MainActivity.this, "Reportes", Toast.LENGTH_SHORT ).show( );
+		Intent i = new Intent( this, GraphicsReports.class );
+		startActivity( i );
+	}
+
+	private void signIn ( ) {
+		Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent( mGoogleApiClient );
+		startActivityForResult( signInIntent, RC_SIGN_IN );
+		Toast.makeText( this, "Ingresaste...! :)", Toast.LENGTH_SHORT ).show( );
+	}
+
+	@Override
+	public void onActivityResult ( int requestCode, int resultCode, Intent data ) {
+		super.onActivityResult( requestCode, resultCode, data );
+		if ( requestCode == RC_SIGN_IN ) {
+			GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent( data );
+			if ( result.isSuccess( ) ) {
+				GoogleSignInAccount account = result.getSignInAccount( );
+				firebaseAuthWithGoogle( account );
+			}
+			else {
+//                updateUI(null);
+			}
+		}
+	}
+
+
+	private void firebaseAuthWithGoogle ( GoogleSignInAccount acct ) {
+		Log.d( TAG, "firebaseAuthWithGoogle:" + acct.getId( ) );
+
+		AuthCredential credential = GoogleAuthProvider.getCredential( acct.getIdToken( ), null );
+		mAuth.signInWithCredential( credential )
+						.addOnCompleteListener( this, new OnCompleteListener< AuthResult >( ) {
+							@Override
+							public void onComplete ( @NonNull Task< AuthResult > task ) {
+								Log.d( TAG, "signInWithCredential:onComplete:" + task.isSuccessful( ) );
+
+								// If sign in fails, display a message to the user. If sign in succeeds
+								// the auth state listener will be notified and logic to handle the
+								// signed in user can be handled in the listener.
+								if ( !task.isSuccessful( ) ) {
+									Log.w( TAG, "signInWithCredential", task.getException( ) );
+									Toast.makeText( MainActivity.this, "Authentication failed.",
+									                Toast.LENGTH_SHORT ).show( );
+								}
+								// ...
+							}
+						} );
+	}
+
+
+	@OnClick(R.id.btnPrueba)
+	public void mensaje(View view) {
+		Toast.makeText( this, "Ingreso", Toast.LENGTH_SHORT ).show( );
+	}
 
 
 }
